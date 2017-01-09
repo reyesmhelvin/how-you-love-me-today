2fa783b0903607c37464308ebd02bb3ee2831042
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-01-09 21:07:40 +0800

    apply props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Row className="App">
         <Header title="How Eriz love me today"/>
-        <Moment />
+        <Moment image={IMG_20170107_153606} date="January 08, 2017" desc="lorem ipsum"/>
       </Row>
     );
   }
@@ -31,9 +31,9 @@ class Moment extends Component {
   render() {
     return (
       <Col s={1} m={6} l={4} className="Moment" > 
-        <Card header={<CardTitle reveal image={IMG_20170107_153606} waves='reorder'/>}
-            title="January 8, 2017"
-            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+        <Card header={<CardTitle reveal image={this.props.image} waves='reorder'/>}
+            title={this.props.date}
+            reveal={this.props.desc}>
             <p>January 8, 2017/ Tuesday</p>
         </Card>
       </Col>
